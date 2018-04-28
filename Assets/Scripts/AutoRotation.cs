e242639ff813980039d682bbf4d228906122e281
ivyxuan <iloveivyxuan@gmail.com>
2018-04-28 21:47:17 +0800

    Add animation to the menu

---
+++ Assets/Scripts/AutoRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotation : MonoBehaviour {
+    private Vector3 updateRotation;
+	
+	// Update is called once per frame
+	void Update () {
+        updateRotation = new Vector3(0.1f, 0.2f, 0.3f);
+        transform.localRotation *= Quaternion.Euler(updateRotation);
+	}
+}
