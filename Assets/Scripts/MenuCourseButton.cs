85b9c573e1918bbf3261107fa94b5a1de09cbaa9
ivyxuan <iloveivyxuan@gmail.com>
2018-04-28 21:46:14 +0800

    Add animation of menu

---
+++ Assets/Scripts/MenuCourseButton.cs
@@ -0,0 +1,14 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class MenuCourseButton : MonoBehaviour, IInputClickHandler {
+    public GameObject Buttons;
+    public GameObject Graph;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        iTween.MoveBy(Buttons, iTween.Hash("y", -0.25, "easeType", "easeInOutExpo", "delay", .1));
+        iTween.RotateBy(Buttons, iTween.Hash("x", .15, "easeType", "easeInOutBack", "delay", .1));
+        iTween.MoveBy(Graph, iTween.Hash("y", 0.25, "easeType", "easeInOutExpo", "delay", .1));
+    }
+}
