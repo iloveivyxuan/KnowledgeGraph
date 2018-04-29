409dcb0e8beb29d47fecee3ea7537503db47920f
ivyxuan <iloveivyxuan@gmail.com>
2018-04-29 17:48:19 +0800

    Add Course Cards Spread animation

--- Assets/Scripts/MenuCourseButton.cs
+++ Assets/Scripts/MenuCourseButton.cs
@@ -4,11 +4,41 @@ using UnityEngine;
 public class MenuCourseButton : MonoBehaviour, IInputClickHandler {
     public GameObject Buttons;
     public GameObject Graph;
+    public GameObject CourseCards;
+    private bool onMenu;
+
+    void Start()
+    {
+        onMenu = true;
+    }
 
     public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if (onMenu)
+        {
+            MenuOpen();
+            CourseCards.SetActive(true);
+            CourseCards.GetComponent<CourseCardsAnimation>().AppearCards();
+            CourseCards.GetComponent<CourseCardsAnimation>().SpreadCards();
+        }
+        else
+        {
+            MenuClose();
+        }
+        onMenu = !onMenu;
+    }
+
+    private void MenuOpen()
     {
         iTween.MoveBy(Buttons, iTween.Hash("y", -0.25, "easeType", "easeInOutExpo", "delay", .1));
         iTween.RotateBy(Buttons, iTween.Hash("x", .15, "easeType", "easeInOutBack", "delay", .1));
         iTween.MoveBy(Graph, iTween.Hash("y", 0.25, "easeType", "easeInOutExpo", "delay", .1));
+    }
+
+    private void MenuClose()
+    {
+        iTween.MoveBy(Buttons, iTween.Hash("y", 0.25, "easeType", "easeInOutExpo", "delay", .1));
+        iTween.RotateBy(Buttons, iTween.Hash("x", -0.15, "easeType", "easeInOutBack", "delay", .1));
+        iTween.MoveBy(Graph, iTween.Hash("y", -0.25, "easeType", "easeInOutExpo", "delay", .1));
     }
 }
