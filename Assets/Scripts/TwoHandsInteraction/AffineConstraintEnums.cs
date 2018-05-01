a2ea0dea159761f3c28b392fe9486970c97203c7
ivyxuan <iloveivyxuan@gmail.com>
2018-05-01 17:40:04 +0800

    Add Two hands interactions

---
+++ Assets/Scripts/TwoHandsInteraction/AffineConstraintEnums.cs
@@ -0,0 +1,25 @@
+namespace HoloToolkit.Unity.InputModule.Utilities.Interactions
+{
+    /// <summary>
+    /// Enum for Axis Constraints
+    /// </summary>
+    public enum AxisConstraint
+    {
+        /// <summary>
+        /// Transform all axes.
+        /// </summary>
+        None,
+        /// <summary>
+        /// Constrain to X-Axis.
+        /// </summary>
+        XAxisOnly,
+        /// <summary>
+        /// Constrain to Y-Axis.
+        /// </summary>
+        YAxisOnly,
+        /// <summary>
+        /// Constrain to Z-Axis.
+        /// </summary>
+        ZAxisOnly
+    };
+}
