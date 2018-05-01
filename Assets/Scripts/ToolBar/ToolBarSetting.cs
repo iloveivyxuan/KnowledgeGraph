f0050e6769b3fe1f5ffee38deb0e53164c423903
ivyxuan <iloveivyxuan@gmail.com>
2018-05-01 21:54:46 +0800

    Add ToolBar position setting script

---
+++ Assets/Scripts/ToolBar/ToolBarSetting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolBarSetting : MonoBehaviour {
+    public List<GameObject> Children;
+    private int index;
+
+    public void GetChildren()
+    {
+        Children = new List<GameObject>();
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.activeSelf)
+            {
+                Children.Add(child.gameObject);
+            }
+        }
+    }
+
+    public void SetToolsPosition()
+    {
+        for (int i = 0; i < Children.Count; i++)
+        {
+            if (Children[i].activeSelf)
+            {
+                Children[i].transform.localPosition = new Vector3(0.0802f * i - Children.Count * 0.0802f / 2f, 0, 0);
+                Debug.Log(0.0802f * i - Children.Count * 0.0802f / 2f);
+            }
+        }
+    }
+}
