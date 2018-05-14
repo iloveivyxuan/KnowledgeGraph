97b50a3ff9b96995f62f2942927aacce6652a489
ivyxuan <iloveivyxuan@gmail.com>
2018-05-15 05:16:40 +0800

    Refactor the menu stack button and back to menu button

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
+﻿using HoloToolkit.Unity;
+using UnityEngine;
 
 public class Menu : MonoBehaviour {
     public bool IsOnMenuState;
     public GameObject ButtonsWrapper;
     public GameObject GraphWrapper;
+    public GameObject InfoGraph;
 
     private void Start()
     {
@@ -22,6 +24,24 @@ public class Menu : MonoBehaviour {
             CloseMenu();
             changeMenuStatus();
         }
+    }
+
+    public void Appear()
+    {
+        changeMenuStatus();
+        gameObject.GetComponent<Tagalong>().enabled = true;
+        iTween.ScaleTo(ButtonsWrapper, iTween.Hash("x", 1, "y", 1, "z", 1, "easeType", "easeInOutExpo", "delay", 0.1));
+        iTween.MoveTo(ButtonsWrapper, iTween.Hash("position", new Vector3(0, 0, 0), "islocal", true, "easeType", "easeInOutExpo", "delay", .2));
+        iTween.MoveTo(InfoGraph, iTween.Hash("position", new Vector3(0, 0.16f, 0), "islocal", true, "easeType", "easeInOutExpo", "delay", .2));
+        iTween.ScaleTo(InfoGraph, iTween.Hash("x", 0.005, "y", 0.005, "z", 0.005, "easeType", "easeInOutExpo", "delay", 0.2));
+    }
+
+    public void Disappear()
+    {
+        changeMenuStatus();
+        iTween.ScaleTo(ButtonsWrapper, iTween.Hash("x", 0, "y", 0, "z", 0, "easeType", "easeInOutExpo", "delay", 0.1));
+        iTween.ScaleTo(InfoGraph, iTween.Hash("x", 0, "y", 0, "z", 0, "easeType", "easeInOutExpo", "delay", 0.1));
+        gameObject.GetComponent<Tagalong>().enabled = false;
     }
 
     private void SplitMenu()
