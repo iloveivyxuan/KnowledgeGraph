70fb67b01f8d3bab9650af3a218bcf60d7dfb5e6
ivyxuan <iloveivyxuan@gmail.com>
2018-05-15 04:00:36 +0800

    Refactor the menu course button script

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -1,10 +1,45 @@
 ﻿using UnityEngine;
 
 public class Menu : MonoBehaviour {
-    public bool onMenu;
+    public bool IsOnMenuState;
+    public GameObject ButtonsWrapper;
+    public GameObject GraphWrapper;
 
     private void Start()
     {
-        onMenu = true;
+        IsOnMenuState = true;
+    }
+
+    public void ChangeSplitStatus()
+    {
+        if (IsOnMenuState)
+        {
+            SplitMenu();
+            changeMenuStatus();
+        }
+        else
+        {
+            CloseMenu();
+            changeMenuStatus();
+        }
+    }
+
+    private void SplitMenu()
+    {
+        iTween.MoveBy(ButtonsWrapper, iTween.Hash("y", -0.25, "easeType", "easeInOutExpo", "delay", .1));
+        iTween.RotateBy(ButtonsWrapper, iTween.Hash("x", .15, "easeType", "easeInOutBack", "delay", .1));
+        iTween.MoveBy(GraphWrapper, iTween.Hash("y", 0.25, "easeType", "easeInOutExpo", "delay", .1));
+    }
+
+    private void CloseMenu()
+    {
+        iTween.MoveBy(ButtonsWrapper, iTween.Hash("y", 0.25, "easeType", "easeInOutExpo", "delay", .1));
+        iTween.RotateBy(ButtonsWrapper, iTween.Hash("x", -0.15, "easeType", "easeInOutBack", "delay", .1));
+        iTween.MoveBy(GraphWrapper, iTween.Hash("y", -0.25, "easeType", "easeInOutExpo", "delay", .1));
+    }
+
+    private void changeMenuStatus()
+    {
+        IsOnMenuState = !IsOnMenuState;
     }
 }
--- Assets/Scripts/Menu/MenuCourseButton.cs
+++ Assets/Scripts/Menu/MenuCourseButton.cs
@@ -3,37 +3,19 @@ using UnityEngine;
 
 public class MenuCourseButton : MonoBehaviour, IInputClickHandler {
     public GameObject Menu;
-    public GameObject Buttons;
-    public GameObject Graph;
     public GameObject CourseCards;
 
     public void OnInputClicked(InputClickedEventData eventData)
     {
-        Menu.GetComponent<Menu>().onMenu = !Menu.GetComponent<Menu>().onMenu;
-        if (!Menu.GetComponent<Menu>().onMenu)
+        Menu.GetComponent<Menu>().ChangeSplitStatus();
+        if (!Menu.GetComponent<Menu>().IsOnMenuState)
         {
-            MenuOpen();
-            CourseCards.GetComponent<CourseCardsAnimation>().AppearCards();
-            CourseCards.GetComponent<CourseCardsAnimation>().SpreadCards();
+            CourseCards.GetComponent<CourseCardsAnimation>().Appear();
+            CourseCards.GetComponent<CourseCardsAnimation>().Spread();
         }
         else
         {
             CourseCards.GetComponent<CourseCardsAnimation>().PutTogether();
-            MenuClose();
         }
-    }
-
-    private void MenuOpen()
-    {
-        iTween.MoveBy(Buttons, iTween.Hash("y", -0.25, "easeType", "easeInOutExpo", "delay", .1));
-        iTween.RotateBy(Buttons, iTween.Hash("x", .15, "easeType", "easeInOutBack", "delay", .1));
-        iTween.MoveBy(Graph, iTween.Hash("y", 0.25, "easeType", "easeInOutExpo", "delay", .1));
-    }
-
-    private void MenuClose()
-    {
-        iTween.MoveBy(Buttons, iTween.Hash("y", 0.25, "easeType", "easeInOutExpo", "delay", .1));
-        iTween.RotateBy(Buttons, iTween.Hash("x", -0.15, "easeType", "easeInOutBack", "delay", .1));
-        iTween.MoveBy(Graph, iTween.Hash("y", -0.25, "easeType", "easeInOutExpo", "delay", .1));
     }
 }
