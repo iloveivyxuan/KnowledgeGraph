c92bc673ca9e45dd595e5f2566ca6ed7121be2d2
ivyxuan <iloveivyxuan@gmail.com>
2018-05-18 04:35:55 +0800

    Refactor the CourseCardInteraction Script

--- Assets/Scripts/Course/CourseCardInteraction.cs
+++ Assets/Scripts/Course/CourseCardInteraction.cs
@@ -1,14 +1,24 @@
-﻿using HoloToolkit.Unity.InputModule;
+﻿using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity;
 using UnityEngine;
 
 public class CourseCardInteraction : MonoBehaviour, IInputClickHandler, IFocusable {
     public GameObject WordsManager;
     public float DefaultHeight;
     public GameObject InfoGraph;
+    public GameObject Menu;
+    public GameObject CourseCards;
+    private bool cardOnShow;
+
+    void Start()
+    {
+        cardOnShow = false;
+    }
 
     public void OnFocusEnter()
     {
-        iTween.ScaleAdd(gameObject, iTween.Hash("x", 0.0001, "y", 0.0001, "easeType", "easeInOutExpo"));
+        iTween.ScaleAdd(gameObject, iTween.Hash("x", 0.0001, "y", 0.0001, "easeType", "easeInOutExpo", "delay", 0));
     }
 
     public void OnFocusExit()
@@ -18,6 +28,60 @@ public class CourseCardInteraction : MonoBehaviour, IInputClickHandler, IFocusab
 
     public void OnInputClicked(InputClickedEventData eventData)
     {
-        DefaultHeight = WordsManager.GetComponent<WordsManager>().GetGraphTransitionHeight(GetComponent<CourseInfo>().Words);
+        cardOnShow = !cardOnShow;
+        if (cardOnShow)
+        {
+            DefaultHeight = WordsManager.GetComponent<WordsManager>().GetGraphTransitionHeight(GetComponent<CourseInfo>().Words);
+            ChangeInfoGraph();
+            OnlyAppearClickedCard(gameObject);
+            Menu.GetComponent<Tagalong>().enabled = false;
+        }
+        else
+        {
+            //DefaultHeight = WordsManager.GetComponent<WordsManager>().GetGraphTransitionHeight(GetComponent<CourseInfo>().Words);
+            RestoreInfoGraph();
+            RestoreCards();
+            Menu.GetComponent<Tagalong>().enabled = true;
+        }
+    }
+
+    private void ChangeInfoGraph()
+    {
+        InfoGraph.GetComponent<SphereCollider>().enabled = false;
+        iTween.MoveTo(InfoGraph, iTween.Hash("x", -0.22f, "y", -0.26f, "islocal", true, "easeType", "easeInOutExpo", "delay", .1));
+        iTween.ScaleTo(InfoGraph, iTween.Hash("x", 0.01, "y", 0.01, "z", 0.01, "easeType", "easeInOutExpo", "delay", 0.1));
+    }
+
+    private void RestoreInfoGraph()
+    {
+        InfoGraph.GetComponent<SphereCollider>().enabled = true;
+        iTween.MoveTo(InfoGraph, iTween.Hash("x", 0f, "y", 0.16f, "islocal", true, "easeType", "easeInOutExpo", "delay", .1));
+        iTween.ScaleTo(InfoGraph, iTween.Hash("x", 0.005, "y", 0.005, "z", 0.005, "easeType", "easeInOutExpo", "delay", 0.1));
+    }
+
+    private void OnlyAppearClickedCard(GameObject clickedCard)
+    {
+        List<GameObject> cards = CourseCards.GetComponent<CourseCardsAnimation>().Children;
+        foreach (GameObject card in cards)
+        {
+            if (card != clickedCard)
+            {
+                card.SetActive(false);
+            }
+        }
+        GetComponent<RectTransform>().localPosition = new Vector3(0.15f, 0, -0.39f);
+    }
+
+    private void RestoreCards()
+    {
+        List<GameObject> cards = CourseCards.GetComponent<CourseCardsAnimation>().Children;
+        foreach (GameObject card in cards)
+        {
+            if (card != gameObject)
+            {
+                card.SetActive(true);
+            }
+        }
+        GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
     }
 }
