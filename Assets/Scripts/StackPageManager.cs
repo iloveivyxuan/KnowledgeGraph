8f17441325768808f51d9e8e4952632787714392
ivyxuan <iloveivyxuan@gmail.com>
2018-05-21 17:47:43 +0800

    Control keywords wrapper tagalong

---
+++ Assets/Scripts/StackPageManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity;
+using UnityEngine;
+
+public class StackPageManager : MonoBehaviour {
+    public GameObject Keyboard;
+    public GameObject KeywordsWrapper;
+
+	void Update () {
+        if (Keyboard.activeSelf)
+        {
+            KeywordsWrapper.GetComponent<Tagalong>().enabled = false;
+        }
+        else
+        {
+            KeywordsWrapper.GetComponent<Tagalong>().enabled = true;
+        }
+	}
+}
