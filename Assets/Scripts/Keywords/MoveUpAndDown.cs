f1e4fd3ef2b4d2e42eeebfd5d3bcc1a73ec74816
ivyxuan <iloveivyxuan@gmail.com>
2018-05-21 20:35:40 +0800

    Add move up down animation to Keyword

---
+++ Assets/Scripts/Keywords/MoveUpAndDown.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class MoveUpAndDown : MonoBehaviour {
+
+	void Update () {
+        iTween.MoveBy(gameObject, iTween.Hash("y", 0.01, "loopType", "pingPong", "delay", 0));
+    }
+}
